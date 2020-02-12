978952249b2aa59ff1a9b660ea7ae1c565652d77
ninjafat <davide.mezzetti@hotmail.it>
2020-02-12 01:40:10 +0100

    completed user stories: #content from 1 to 10 #timer from 1 to 9, 12, 14 #audio 1, 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const AppControl = ({  onClick, type, value}) => {
 
   return (
     <div className="appSetting" >
-      <p id={type+"-label"} className="settingTitle">{type} Length</p>
+      <div id={type+"-label"} className="settingTitle">{type} Length</div>
 
       <button onClick={(e)=>{onClick(e, type, value-1)}} id={type+"-decrement"}>-</button>
       <div className="settingValue" id={type+"-length"}>{value}</div>
@@ -15,61 +15,171 @@ const AppControl = ({  onClick, type, value}) => {
   )
 }
 
+function format(val) {
+  if(val > -1 && val < 10) return "0"+val
+  return val+"";
+}
+
+function convertTimeLeft(timeSec){
+  let time_string = "";
+  let min = parseInt(timeSec / 60);
+  let sec = parseInt(timeSec % 60);
+  time_string =  format(min) + ":" + format(sec);
+  return time_string;
+}
+
 /**
  * time  mm:ss format (i.e. 25:00).
  */
-const Display=()=>{
+const Display=({timeLeft, rangeActive})=>{
+  let time = convertTimeLeft(timeLeft);
   return(
     <div className="displayTimer">
-      <p id="timer-label">Session</p>
-      <p id="time-left">18:45</p>
+      <p id="timer-label">{rangeActive}</p>
+      <p id="time-left">{time}</p>
     </div>
   )
 }
 
 const initialConf = {
-  breakLength : 5,
-  sessionLength: 25,
-  timeLeft: "25:00"
+  break: 5, //min
+  session: 25, //min
+  timeLeft: 1500 //sec
 }
+
+const _SESSION = "Session";
+const _BREAK = "Break";
+const _MIN = 0; /** 0 min */
+const _MAX = 60; /** 60 min */
+const _1SEC = 1;
+/** */
+function isValid(min, max, value){
+  if(value === min || value > max) return false;
+  return true;
+}
+
 
 class App extends React.Component {
 
   constructor(){
     super();
     this.state = {
-      break: initialConf.breakLength,
-      session: initialConf.sessionLength,
+      break: initialConf.break,
+      session: initialConf.session,
       timeLeft: initialConf.timeLeft,
       isRunning: false,
+      rangeActive: _SESSION
     }
+    this.timer = null;
+    this.beep = React.createRef();
   }
 
+
+  /** 
+   * newValue is integer  minute
+   */
   handleSettings = (e, type, newValue) => {
     /** check */
     e.preventDefault();
     e.stopPropagation();
-    if(this.state.isRunning) return
+    if (this.state.isRunning) return;
+    if(!isValid(_MIN, _MAX, newValue)) return;
+    let updatedTimeLeft = this.state.timeLeft;
 
+    if( this.state.rangeActive === _SESSION && type ===  "session"){
+      updatedTimeLeft = newValue * 60;
+    }
+    if(this.state.rangeActive === _BREAK && type === "break") {
+      updatedTimeLeft = newValue * 60;
+    }
+    
     return this.setState({
-      [type]: newValue
+      [type]: newValue,
+      timeLeft: updatedTimeLeft
     })
   }
 
-  handlePlay = () => {
-
+  nextRange=()=>{
+    if( this.state.rangeActive === _SESSION) return _BREAK;
+    if(this.state.rangeActive === _BREAK) return _SESSION;
   }
 
-  handlePause = () => {
-
+/**
+ * break, session are minutes
+ * @return seconds
+ */
+  nextTimeLeft=()=>{
+    if( this.state.rangeActive === _SESSION) return this.state.break * 60 ;
+    if( this.state.rangeActive === _BREAK) return this.state.session * 60 ;
   }
 
-  handleReset = () => {
-    /** delete running timer and set to initial params */
+  playSound=()=>{
+    this.beep.current.play();
+  }
+
+  /** start from the value displayed and the range active*/
+  handlePlay = (e) => {
+    if(e){
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    const { isRunning, timeLeft, isPaused } = this.state;
+    const _self = this;
+    if (isRunning && !isPaused) return this.handlePause();
+    //if(this.state.isPaused)
+   
+    this.timer = setInterval( function(){
+     
+        /** START NEXT POMODORO */
+        if (_self.state.timeLeft === 0 ){
+            clearInterval(_self.timer);
+            let nextRange =  _self.nextRange();
+            let nextRangeTime =  _self.nextTimeLeft();
+            _self.playSound();
+            
+            _self.setState({
+              rangeActive: nextRange,
+              timeLeft: nextRangeTime,
+              isRunning: false,
+            }, function(){
+              _self.handlePlay();
+            })
+        }
+
+        let nextTimeLeft = _self.state.timeLeft - 1;
+
+        _self.setState({
+          timeLeft: nextTimeLeft,
+          isRunning: true
+        })
+    }, 1000 )
+  }
+
+  handlePause = (e) => {
+    if(e){
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
     this.setState({
-      breakLength: initialConf.breakLength,
-      sessionLength: initialConf.sessionLength,
-      timeLeft: initialConf.timeLeft
+      isPaused: true
+    })
+  }
+
+  handleReset = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    /** delete running timer and set to initial params */
+    if(this.state.isRunning){
+      clearInterval(this.timer);
+    }
+    this.setState({
+      break: initialConf.break,
+      session: initialConf.session,
+      timeLeft: initialConf.timeLeft,
+      isRunning: false,
+      rangeActive: _SESSION
     })
     return
   }
@@ -80,15 +190,16 @@ class App extends React.Component {
       <div className="App">
         <div className="container">
           <h1 className="title">Pomodoro Clock</h1>
+          <audio id="beep" src="./horse.mp3" ref={this.beep}></audio>
           <div className="settingsContainer">
             <AppControl type="break"   onClick={this.handleSettings} value={this.state.break} />
             <AppControl type="session"  onClick={this.handleSettings} value= {this.state.session} />
           </div>
-          <Display />
+          <Display timeLeft={this.state.timeLeft} rangeActive={this.state.rangeActive}/>
           <div className="timerControls">
-          <button onClick={this.handlePlay} id="start_stop">start</button>
-          <button onClick={this.handlePause} id="">pause</button>
-          <button onClick={this.handleReset} id="reset">reset</button>
+          <button onClick={(e)=>{this.handlePlay(e)}} id="start_stop">start</button>
+          <button onClick={(e)=>{this.handlePause(e)}} id="">pause</button>
+          <button onClick={(e)=>{this.handleReset(e)}} id="reset">reset</button>
           </div>
         </div>  
       </div>
